906b04f4056424817e4da60a66354bd3cf96be51
Sumit Sharma <84061534+Su-nlight@users.noreply.github.com>
2025-11-19 09:36:02 +0530

    Add sample output comment to numto.js
    Added sample output as a comment for reference.

--- wt-lab/prac7/numto.js
+++ wt-lab/prac7/numto.js
@@ -21,3 +21,32 @@ inputs.forEach((input) => {
 });
 
 console.log(); // just for a clean ending
+/*
+Sample Output:
+PS C:\Users\LENOVO\Desktop\> node change.js 10 34 56 72 12
+
+Number: 10
+  Hexadecimal: a
+  Octal:       12
+  Binary:      1010
+
+Number: 34
+  Hexadecimal: 22
+  Octal:       42
+  Binary:      100010
+
+Number: 56
+  Hexadecimal: 38
+  Octal:       70
+  Binary:      111000
+
+Number: 72
+  Hexadecimal: 48
+  Octal:       110
+  Binary:      1001000
+
+Number: 12
+  Hexadecimal: c
+  Octal:       14
+  Binary:      1100
+*/
